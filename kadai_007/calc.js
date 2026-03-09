0c390a933c5917344094068dec56171e21a90899
uramasa1321-del <uramasa1321@gmail.com>
2026-03-09 14:05:21 +0900

    kadai_007 アップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,77 @@
+// let num = 10;
+
+// if (num % 3 == 0 && num % 5 == 0) {
+//   console.log('3と5の倍数です');
+// }
+
+//  else if (num % 3 == 0) {
+//   console.log('3の倍数です');
+// }
+
+// else if (num % 5 == 0) {
+//   console.log('5の倍数です');
+// }
+
+// else {
+//   console.log(num)
+// }
+
+
+
+
+// let num = 15;
+
+// if (num % 3 == 0 && num % 5 == 0) {
+//   console.log('3と5の倍数です');
+// }
+
+//  else if (num % 3 == 0) {
+//   console.log('3の倍数です');
+// }
+
+// else if (num % 5 == 0) {
+//   console.log('5の倍数です');
+// }
+
+// else {
+//   console.log(num)
+// }
+
+
+// let num = 45;
+
+// if (num % 3 == 0 && num % 5 == 0) {
+//   console.log('3と5の倍数です');
+// }
+
+//  else if (num % 3 == 0) {
+//   console.log('3の倍数です');
+// }
+
+// else if (num % 5 == 0) {
+//   console.log('5の倍数です');
+// }
+
+// else {
+//   console.log(num)
+// }
+
+
+
+let num = 135;
+
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
+}
+
+ else if (num % 3 == 0) {
+  console.log('3の倍数です');
+}
+
+else if (num % 5 == 0) {
+  console.log('5の倍数です');
+}
+
+else {
+  console.log(num)
+}
